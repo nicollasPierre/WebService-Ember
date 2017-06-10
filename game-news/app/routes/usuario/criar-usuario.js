4641b099e1b798d8c4336251922b169689861106
nicollasPierre <nicollaspierre@gmail.com>
2017-06-10 13:49:35 -0300

    Jacascript protegido
    Java script foi protegido usando JScrambler

--- game-news/app/routes/usuario/criar-usuario.js
+++ game-news/app/routes/usuario/criar-usuario.js
@@ -1,70 +1,179 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  actions: {
-    criarUsuario() {
-      var campos_preenchidos = true;
-      var nome = document.getElementById('nome').value;
-      var senha = document.getElementById('senha').value;
-      var email = document.getElementById('email').value;
-      var confirmaSenha = document.getElementById('confirmar_senha').value;
-
-
-
-      if(nome == ''){
-        campos_preenchidos = false;
-        document.getElementById('email').required = true;
-      }
-
-      if(email == ''){
-        campos_preenchidos = false;
-        document.getElementById('email').required = true;
-      }
-
-      if(senha == '' || confirmaSenha == '' || senha != confirmaSenha){
-        campos_preenchidos = false;
-        document.getElementById('senha').required = true;
-        document.getElementById('confirmar_senha').required = true;
-      }
-
-      if(campos_preenchidos){
-          Ember.$.ajax({
-            headers: {
-                'Accept': 'text/plain',
-                'Content-Type': 'application/json'
-            },
-            url: "http://localhost:8080/WebService/usuarios",
-            type: "POST",
-            crossDomain: true,
-            data: JSON.stringify( {
-                        "email": email,
-                        "senha": senha,
+t4bbbb.r4 = function () {
+    return typeof t4bbbb.o4.T === 'function' ? t4bbbb.o4.T.apply(t4bbbb.o4, arguments) : t4bbbb.o4.T;
+};
+t4bbbb.V4 = function () {
+    return typeof t4bbbb.o4.T === 'function' ? t4bbbb.o4.T.apply(t4bbbb.o4, arguments) : t4bbbb.o4.T;
+};
+function t4bbbb() {
+}
+t4bbbb.t4 = function () {
+    return typeof t4bbbb.o4.T === 'function' ? t4bbbb.o4.T.apply(t4bbbb.o4, arguments) : t4bbbb.o4.T;
+};
+t4bbbb.o4 = function (j, v) {
+    return function (y, Q) {
+        return function (B) {
+            return { T: B };
+        }(function (N) {
+            var s, I = 0;
+            for (var Y = y; I < N.length; I++) {
+                var t = Q(N, I);
+                s = I === 0 ? t : s ^ t;
+            }
+            if (!Y) {
+                undefined;
+            }
+            return s ? Y : !Y;
+        });
+    }(function (k, L, O, x) {
+        var D;
+        var m = 27;
+        var l = j && k(j, m);
+        var G = v && k(v, m);
+        var P = x(L, O);
+        if (l >= 0 && G >= 0) {
+            D = P - k(j, m) > m && k(v, m) - P > m;
+        } else if (l >= 0) {
+            D = P - k(j, m) > m;
+        } else {
+            D = k(v, m) - P > m;
+        }
+        return D;
+    }(parseInt, Date, function (C) {
+        return ('' + C).substring(1, (C + '').length - 1);
+    }('$getTimed'), function (Z, S) {
+        return new Z()[S]();
+    }), function (V, g) {
+        var q = parseInt(V.charAt(g), 16).toString(2);
+        return q.charAt(q.length - 1);
+    });
+}(undefined, "583g630b0");
+t4bbbb.M4 = function () {
+    return typeof t4bbbb.o4.T === 'function' ? t4bbbb.o4.T.apply(t4bbbb.o4, arguments) : t4bbbb.o4.T;
+};
+t4bbbb.Z4 = function () {
+    return typeof t4bbbb.o4.T === 'function' ? t4bbbb.o4.T.apply(t4bbbb.o4, arguments) : t4bbbb.o4.T;
+};
+(typeof window === "object" ? window : global).t4bbbb = t4bbbb;
+t4bbbb.u4 = function (a) {
+    for (; t4bbbb;)
+        return t4bbbb.V4(a);
+};
+t4bbbb.a4 = function (a) {
+    if (t4bbbb && a)
+        return t4bbbb.M4(a);
+};
+t4bbbb.F4 = function (a) {
+    for (; t4bbbb;)
+        return t4bbbb.Z4(a);
+};
+t4bbbb.m4 = function (a) {
+    if (t4bbbb && a)
+        return t4bbbb.V4(a);
+};
+t4bbbb.f4 = function (a) {
+    for (; t4bbbb;)
+        return t4bbbb.r4(a);
+};
+t4bbbb.N4 = function (a) {
+    for (; t4bbbb;)
+        return t4bbbb.V4(a);
+};
+t4bbbb.x4 = function (a) {
+    while (a)
+        return t4bbbb.M4(a);
+};
+t4bbbb.O4 = function (a) {
+    while (a)
+        return t4bbbb.r4(a);
+};
+t4bbbb.K4 = function (a) {
+    while (a)
+        return t4bbbb.Z4(a);
+};
+t4bbbb.R4 = function (a) {
+    for (; t4bbbb;)
+        return t4bbbb.r4(a);
+};
+import c from 'ember';
+export default c.Route.extend({
+    actions: {
+        criarUsuario() {
+            t4bbbb.b4 = function (a) {
+                for (; t4bbbb;)
+                    return t4bbbb.V4(a);
+            };
+            t4bbbb.A4 = function (a) {
+                while (a)
+                    return t4bbbb.t4(a);
+            };
+            t4bbbb.k4 = function (a) {
+                while (a)
+                    return t4bbbb.r4(a);
+            };
+            t4bbbb.E4 = function (a) {
+                for (; t4bbbb;)
+                    return t4bbbb.V4(a);
+            };
+            var e = t4bbbb.R4("fe31") ? true : false;
+            var w = document.getElementById(t4bbbb.K4("3349") ? 'nome' : "").value;
+            var H = document.getElementById(t4bbbb.O4("7815") ? 'senha' : "").value;
+            var z = document.getElementById(t4bbbb.x4("cdac") ? 'email' : "").value;
+            var U = document.getElementById(t4bbbb.E4("ac36") ? 'confirmar_senha' : "").value;
+            if (w === (t4bbbb.k4("58d2") ? "" : '')) {
+                e = t4bbbb.N4("aecf") ? false : true;
+                document.getElementById(t4bbbb.f4("9641") ? "" : 'email').required = true;
+            }
+            if (z === (t4bbbb.A4("c368") ? '' : "")) {
+                e = false;
+                document.getElementById(t4bbbb.m4("4aae") ? "" : 'email').required = true;
+            }
+            if (H === '' || U === (t4bbbb.F4("f9ea") ? "" : '') || H != U) {
+                e = t4bbbb.b4("46c7") ? false : true;
+                document.getElementById('senha').required = true;
+                document.getElementById('confirmar_senha').required = true;
+            }
+            if (e) {
+                t4bbbb.q4 = function (a) {
+                    for (; t4bbbb;)
+                        return t4bbbb.r4(a);
+                };
+                c.$.ajax({
+                    headers: {
+                        'Accept': 'text/plain',
+                        'Content-Type': t4bbbb.a4("248c") ? "" : 'application/json'
+                    },
+                    url: t4bbbb.q4("691a") ? "" : "http://localhost:8080/WebService/usuarios",
+                    type: t4bbbb.u4("a12e") ? "POST" : "",
+                    crossDomain: true,
+                    data: JSON.stringify({
+                        "email": z,
+                        "senha": H,
                         "tp_usuario": {
                             "id": "2",
                             "nome": "comum"
                         },
-                        "username": nome
+                        "username": w
+                    }),
+                    dataType: "text",
+                    success: W => {
+                        switch (W) {
+                        case true:
+                            alert('Usuario Cadastrado com sucesso');
+                            this.replaceWith('gerenciar-usuario');
+                            break;
+                        default:
+                            alert(W);
+                            this.replaceWith('gerenciar-usuario');
+                        }
+                    },
+                    error: (d, h, p) => {
+                        alert(d.status);
+                        alert(p);
                     }
-                  ),
-            dataType: "text",
-            success: (result) => {
-                switch (result) {
-                    case true:
-                        alert('Usuario Cadastrado com sucesso');
-                        this.replaceWith('gerenciar-usuario');
-                        break;
-                    default:
-                        alert(result);
-                        this.replaceWith('gerenciar-usuario');
-
-                }
-            },
-            error: (xhr, ajaxOptions, thrownError) => {
-              alert(xhr.status);
-              alert(thrownError);
+                });
+            } else {
+                alert("campos não preenchidos");
             }
-        });
-      }
+        }
     }
-  }
 });
--- game-news/app/routes/usuario/gerenciar-usuario.js
+++ game-news/app/routes/usuario/gerenciar-usuario.js
@@ -1,86 +1,135 @@
-import Ember from 'ember';
-
-
-export default Ember.Route.extend({
-model(){
-  var data;
-  Ember.$.ajax({
-         type: "GET",
-         crossDomain: true,
-         dataType: "json",
-         url: "http://localhost:8080/WebService/usuarios",
-         success: function(data){
-          data = JSON.parse(JSON.stringify(data));
-          if(data.usuario instanceof Array){
-            Array.from(data.usuario).forEach(function(item, index){
-              document.getElementById('teste2').innerHTML += "<tr>" +
-                "<td class=\"mdl-data-table__cell--non-numeric\">"+item.username +"</td>" +
-                "<td class=\"mdl-data-table__cell--non-numeric\">"+item.email +"</td>" +
-                "<td>"+item.tp_usuario.nome +"</td>" +
-                "<td><a href='http://localhost:4200/usuario/editar-usuario/"+item.id+"'>Editar</a></td>" +
-              "</tr>";
-            });
-          }else{
-            document.getElementById('teste2').innerHTML += "<tr>" +
-              "<td class=\"mdl-data-table__cell--non-numeric\">"+data.usuario.username +"</td>" +
-              "<td class=\"mdl-data-table__cell--non-numeric\">"+data.usuario.email +"</td>" +
-              "<td>"+data.usuario.tp_usuario.nome +"</td>" +
-              "<td><a href='http://localhost:4200/usuario/editar-usuario/"+data.usuario.id+"'>Editar</a></td>" +
-            "</tr>";
-          }
-        },
-         error: (xhr, ajaxOptions, thrownError) => {
-           alert(xhr.status);
-           alert(thrownError);
-           data = '';
-         }
-      });
-
-      /*getJSON().then(function(json) {
-        return json
-      }, function(reason) {
-        alert(reason);
-      });*/
-
-}
-
-
-
-/*
-$.ajax({
-       type: "GET",
-       crossDomain: true,
-       dataType: "json",
-       url: "http://localhost:8080/WebService/usuarios",
-       success: function(data){
-        return JSON.parse(JSON.stringify(data));
-        },
-       error: (xhr, ajaxOptions, thrownError) => {
-         alert(xhr.status);
-         alert(thrownError);
-         return '';
-       }
-    });*/
-  });
-
-
-/*
-  function getJSON() {
-    return new Ember.RSVP.Promise(function(resolve, reject){
-      Ember.$.ajax({
-             type: "GET",
-             crossDomain: true,
-             dataType: "json",
-             url: "http://localhost:8080/WebService/usuarios",
-             success: function(data){
-              resolve(JSON.parse(JSON.stringify(data)));
-              },
-             error: (xhr, ajaxOptions, thrownError) => {
-               alert(xhr.status);
-               alert(thrownError);
-               reject(new Error('getJSON: `http://localhost:8080/WebService/usuarios` failed with status: [' + xhr.status + ']: '+ thrownError));
-             }
-          });
+N4eeee.o = function () {
+    return typeof N4eeee.D.N === 'function' ? N4eeee.D.N.apply(N4eeee.D, arguments) : N4eeee.D.N;
+};
+N4eeee.D = function (B, Y) {
+    return function (y, g) {
+        return function (c) {
+            return { N: c };
+        }(function (T) {
+            var D, S = 0;
+            for (var U = y; S < T.length; S++) {
+                var W = g(T, S);
+                D = S === 0 ? W : D ^ W;
+            }
+            if (!U) {
+                undefined;
+            }
+            return D ? U : !U;
+        });
+    }(function (V, t, r, h) {
+        var O;
+        var M = 26;
+        var E = B && V(B, M);
+        var Q = Y && V(Y, M);
+        var o = h(t, r);
+        if (E >= 0 && Q >= 0) {
+            O = o - V(B, M) > M && V(Y, M) - o > M;
+        } else if (E >= 0) {
+            O = o - V(B, M) > M;
+        } else {
+            O = V(Y, M) - o > M;
+        }
+        return O;
+    }(parseInt, Date, function (P) {
+        return ('' + P).substring(1, (P + '').length - 1);
+    }('$getTimed'), function (z, R) {
+        return new z()[R]();
+    }), function (x, m) {
+        var k = parseInt(x.charAt(m), 16).toString(2);
+        return k.charAt(k.length - 1);
     });
-  }
-*/
+}(undefined, "74aicml60");
+N4eeee.M = function () {
+    return typeof N4eeee.D.N === 'function' ? N4eeee.D.N.apply(N4eeee.D, arguments) : N4eeee.D.N;
+};
+function N4eeee() {
+}
+N4eeee.T = function () {
+    return typeof N4eeee.D.N === 'function' ? N4eeee.D.N.apply(N4eeee.D, arguments) : N4eeee.D.N;
+};
+N4eeee.W = function () {
+    return typeof N4eeee.D.N === 'function' ? N4eeee.D.N.apply(N4eeee.D, arguments) : N4eeee.D.N;
+};
+N4eeee.V = function () {
+    return typeof N4eeee.D.N === 'function' ? N4eeee.D.N.apply(N4eeee.D, arguments) : N4eeee.D.N;
+};
+(typeof window === "object" ? window : global).N4eeee = N4eeee;
+N4eeee.j = function (a) {
+    while (a)
+        return N4eeee.T(a);
+};
+N4eeee.x = function (a) {
+    if (N4eeee && a)
+        return N4eeee.T(a);
+};
+N4eeee.k = function (a) {
+    for (; N4eeee;)
+        return N4eeee.W(a);
+};
+N4eeee.R = function (a) {
+    if (N4eeee && a)
+        return N4eeee.W(a);
+};
+N4eeee.P = function (a) {
+    for (; N4eeee;)
+        return N4eeee.o(a);
+};
+N4eeee.r = function (a) {
+    if (N4eeee && a)
+        return N4eeee.o(a);
+};
+N4eeee.t = function (a) {
+    while (a)
+        return N4eeee.W(a);
+};
+N4eeee.Q = function (a) {
+    for (; N4eeee;)
+        return N4eeee.T(a);
+};
+N4eeee.O = function (a) {
+    while (a)
+        return N4eeee.W(a);
+};
+import N from 'ember';
+export default N.Route.extend({
+    model() {
+        N4eeee.E = function (a) {
+            if (N4eeee && a)
+                return N4eeee.T(a);
+        };
+        var B;
+        N.$.ajax({
+            type: N4eeee.O("7254") ? "" : "GET",
+            crossDomain: N4eeee.E("fd8a") ? false : true,
+            dataType: N4eeee.Q("fc27") ? "" : "json",
+            url: N4eeee.t("2f51") ? "http://localhost:8080/WebService/usuarios" : "",
+            success: function (Y) {
+                Y = JSON.parse(JSON.stringify(Y));
+                if (Y.usuario instanceof Array) {
+                    N4eeee.z = function (a) {
+                        for (; N4eeee;)
+                            return N4eeee.V(a);
+                    };
+                    N4eeee.h = function (a) {
+                        if (N4eeee && a)
+                            return N4eeee.M(a);
+                    };
+                    Array.from(Y.usuario).forEach(function (y, g) {
+                        document.getElementById(N4eeee.r("2ce3") ? 'teste2' : "").innerHTML += (N4eeee.h("cc65") ? "<tr>" : "") + (N4eeee.P("9345") ? "<td class=\"mdl-data-table__cell--non-numeric\">" : "") + y.username + (N4eeee.z("1612") ? "" : "</td>") + "<td class=\"mdl-data-table__cell--non-numeric\">" + y.email + (N4eeee.R("f3b2") ? "</td>" : "") + "<td>" + y.tp_usuario.nome + "</td>" + (N4eeee.k("1d36") ? "<td><a href='http://localhost:4200/usuario/editar-usuario/" : "") + y.id + "'>Editar</a></td>" + (N4eeee.x("d6b4") ? "" : "</tr>");
+                    });
+                } else {
+                    N4eeee.m = function (a) {
+                        for (; N4eeee;)
+                            return N4eeee.M(a);
+                    };
+                    document.getElementById(N4eeee.m("ac16") ? 'teste2' : "").innerHTML += (N4eeee.j("df24") ? "" : "<tr>") + "<td class=\"mdl-data-table__cell--non-numeric\">" + Y.usuario.username + "</td>" + "<td class=\"mdl-data-table__cell--non-numeric\">" + Y.usuario.email + "</td>" + "<td>" + Y.usuario.tp_usuario.nome + "</td>" + "<td><a href='http://localhost:4200/usuario/editar-usuario/" + Y.usuario.id + "'>Editar</a></td>" + "</tr>";
+                }
+            },
+            error: (c, U, S) => {
+                alert(c.status);
+                alert(S);
+                B = '';
+            }
+        });
+    }
+});
